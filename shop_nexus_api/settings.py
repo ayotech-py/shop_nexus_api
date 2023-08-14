eb3243399ec8905830a86ca823306c198d5d8203
ayotech-py <ajaomalik2018@gmail.com>
2023-08-14 02:00:08 +0100

    updated

--- shop_nexus_api/settings.py
+++ shop_nexus_api/settings.py
@@ -180,4 +180,4 @@ PAYSTACK_SECRET_KEY = os.environ["PAYSTACK_SECRET_KEY"]
 PAYSTACK_PUBLIC_KEY = os.environ["PAYSTACK_PUBLIC_KEY"]
 
 
-WSGI_APPLICATION = "shop_nexus_api.wsgi.app"
+WSGI_APPLICATION = "shop_nexus_api.wsgi.application"
