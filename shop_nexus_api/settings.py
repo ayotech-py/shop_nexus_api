71c1447bb08f820fffdd1565dcea25f4a21b57ec
ayotech-py <ajaomalik2018@gmail.com>
2023-10-12 02:22:02 +0100

    michoice

--- shop_nexus_api/settings.py
+++ shop_nexus_api/settings.py
@@ -13,9 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
 import os
-import cloudinary
-import cloudinary.api
-import cloudinary.uploader
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
