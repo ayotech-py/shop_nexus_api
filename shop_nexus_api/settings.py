a14046eb5b257f16800e867b4766807a35bb7e49
ayotech-py <ajaomalik2018@gmail.com>
2023-10-12 02:38:52 +0100

    michoice

--- shop_nexus_api/settings.py
+++ shop_nexus_api/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
 import os
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
